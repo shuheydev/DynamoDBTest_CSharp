6040159b3f44091cf563d7aca13b01deb778d343
shuhey <shuhey.dev@gmail.com>
2018-11-09 10:11:28 +0900

    プロジェクト ファイルを追加します。

---
+++ DynamoDBTest/Function.cs
@@ -0,0 +1,32 @@
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace DynamoDBTest
+{
+    public class Function
+    {
+
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<SkillResponse> FunctionHandler(SkillRequest input, ILambdaContext context)
+        {
+            //ここにDynamoDBにアクセスするコードを追加していく
+
+            return new SkillResponse
+            {
+                Version = "",
+                Response = new ResponseBody()
+            };
+        }
+    }
+}
